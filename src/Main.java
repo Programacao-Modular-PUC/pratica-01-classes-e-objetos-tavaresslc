3b864410f6b75448c6cc1b8c168db4f297549bbc
Lucas Tavares <lucastavares004@gmail.com>
2026-03-05 19:03:57 -0300

    feat: exercicio01

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        TrianguloRetangulo t = new TrianguloRetangulo();
+
+        t.setC1(readDouble(sc, "Digite o valor do primeiro cateto: "));
+        t.setC2(readDouble(sc, "Digite o valor do segundo cateto: "));
+        t.setH(t.calcHypotenuse());
+        t.setA(t.calcArea());
+
+        System.out.println("Hipotenusa: " + t.getH() + " Area: " + t.getA());
+    }
+
+    public static double readDouble(Scanner sc, String message) {
+        while (true) {
+            System.out.print(message);
+
+            try {
+                return Double.parseDouble(sc.next().trim().replace(",", "."));
+            } catch (NumberFormatException e) {
+                System.out.println("Valor inválido. Digite um numero decimal valido.");
+            }
+        }
     }
 }
